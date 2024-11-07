7d083e7ac65d8fed92522c12468595b20a7ffd02
Miguel03br <167449074+Miguel03br@users.noreply.github.com>
2024-11-07 14:36:28 -0300

    Add files via upload

---
+++ storage/framework/views/3d87bc15cb04d089e7220470560dcf42.php
@@ -0,0 +1,85 @@
+
+
+
+<?php $__env->startSection('content'); ?>
+
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<nav class="navbar navbar-dark bg-dark fixed-top">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Construções</a>
+    <img src="<?php echo e(asset('images/user.png')); ?>" alt="" width="60px" height="60px" style="border-radius: 50%;">
+    <div class="offcanvas offcanvas-start text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">ConstruControl</h5>
+        <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Construções</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Funcinários</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Avisos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<table class="table " style="margin-top: 10%;">
+
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nome</th>
+      <th scope="col">Data de finalização</th>
+      <th scope="col">Solicitação de materiais</th>
+      <th scope="col">Opções</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php $__currentLoopData = $constructions; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $Construction): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <tr>
+      <th scope="row"><?php echo e($Construction->id); ?></th>
+      <td>
+        
+        <?php echo e($Construction->nome); ?>
+
+        
+      </td>
+        
+     <td>
+        <?php echo e($Construction->data); ?>
+
+     </td>
+
+     <td>
+        <?php echo e($Construction->solicitação); ?>
+
+     </td>
+
+     <td>
+     <button type="button" class="btn btn-danger">Deletar</button>
+     <button type="button" class="btn btn-warning">Editar</button>
+     </td>
+    </tr>
+  <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?> 
+  </tbody>
+</table>
+
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Aluno\construLaravel\resources\views/constructions.blade.php ENDPATH**/ ?>
